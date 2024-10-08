587c829ced41c50d40c6083d4479941d8688fcc3
Byungwook <byungwook91.jeon@lgepartner.com>
2024-10-08 16:37:14 +0900

    1. SecurityContext Log 추적 메소드 추가

--- src/main/java/com/jbproject/jutopia/config/security/util/SecurityUtils.java
+++ src/main/java/com/jbproject/jutopia/config/security/util/SecurityUtils.java
@@ -3,7 +3,9 @@ package com.jbproject.jutopia.config.security.util;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jbproject.jutopia.config.security.constant.JwtTokenConstants;
 import com.jbproject.jutopia.config.security.constant.SecurityErrorCode;
+import com.jbproject.jutopia.config.security.jwt.AccessJwtToken;
 import com.jbproject.jutopia.config.security.jwt.JwtTokenInfo;
+import com.jbproject.jutopia.config.security.jwt.RefreshJwtToken;
 import com.jbproject.jutopia.exception.ErrorCode;
 import com.jbproject.jutopia.exception.model.ExceptionModel;
 import io.jsonwebtoken.ExpiredJwtException;
@@ -17,6 +19,9 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.io.IOException;
 
@@ -60,5 +65,24 @@ public class SecurityUtils {
         String body = new ObjectMapper().writeValueAsString(exceptionModel);
         request.setAttribute("errorBody", body);
         request.getRequestDispatcher("/error/auth").forward(request, response);
+    }
+
+
+     public static void logSecurityContext(String message) {
+        SecurityContext context = SecurityContextHolder.getContext();
+        Authentication authentication = context.getAuthentication();
+
+        if (authentication != null) {
+            System.out.println(message);
+            System.out.println("Authentication class: " + authentication.getClass().getName());
+            System.out.println("Is authenticated: " + authentication.isAuthenticated());
+            System.out.println("Authorities: " + authentication.getAuthorities());
+            System.out.println("Principal: " + authentication.getPrincipal());
+            System.out.println("SecurityContext hash: " + System.identityHashCode(context));
+            System.out.println("authentication instanceof AccessJwtToken : " + (authentication instanceof AccessJwtToken));
+            System.out.println("authentication instanceof RefreshJwtToken : " + (authentication instanceof RefreshJwtToken));
+        } else {
+            System.out.println(message + ": No authentication present");
+        }
     }
 }
