4ba5f14530c38ecefa0213447590adc81e7cb517
Byungwook <byungwook91.jeon@lgepartner.com>
2024-10-08 11:19:16 +0900

    1. default url : home main 추가

--- src/main/java/com/jbproject/jutopia/config/security/SecurityConfig.java
+++ src/main/java/com/jbproject/jutopia/config/security/SecurityConfig.java
@@ -45,7 +45,7 @@ public class SecurityConfig {
             "/swagger-resources/**", "/configuration/ui", "/configuration/security",
             "/swagger-ui.html", "/webjars/**", "swagger v3",
             "/v3/api-docs/**", "/swagger-ui/**", "/favicon.ico",
-            "/auth/**","/error/**"
+            "/auth/**","/error/**","/home/main"
     };
 
     // 기본 허용 path - request Matcher 추가
